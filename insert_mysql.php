5d9d83bd3510b5aba5cd0366746bcca5c3092c2f
CitiaTechologies <165090795+CitiaTechologies@users.noreply.github.com>
2024-03-27 11:41:20 -0600

    Update insert_mysql.php

--- insert_mysql.php
+++ insert_mysql.php
@@ -20,7 +20,7 @@ if (mysql_connect($localhost, $username, $password))
   	@mysql_select_db($database) or die ("Unable to select database");
 
     // Next two lines will write into your table 'test_table_name_here' with 'yourdata' value from the arduino and will timestamp that data using 'now()'
-    $query = "INSERT INTO $tablename VALUES ($yourdata,now())";
+    $query = "INSERT INTO $tablename(humedad,fecha) VALUES($yourdata,now())";
   	$result = mysql_query($query);
   } else {
   	echo('Unable to connect to database.');
